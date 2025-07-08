015167269e838bb3dce0bed582f7a58726e7de6d
Cristian Acevedo <crashedthe@gmail.com>
2025-07-08 08:24:19 -0500

    actualizacion de las imagenes

--- views/productos/categoria.php
+++ views/productos/categoria.php
@@ -8,7 +8,7 @@ require_once './includes/HeaderLogout.php'; // Incluye el encabezado con el men�
     <?php if ($productos && mysqli_num_rows($productos) > 0): ?> <!-- Verifica si hay productos en la categoría -->
         <?php while ($prod = mysqli_fetch_assoc($productos)): ?> <!-- Itera sobre cada producto -->
             <div class="bg-neutral-900 p-2 rounded">
-                <img src="uploads/<?= $prod['imagen'] ?>" alt="<?= $prod['nombre'] ?>" class="rounded w-full h-48 object-cover"> <!-- Muestra la imagen del producto -->
+                <img src="<?= $prod['imagen'] ?>" alt="<?= $prod['nombre'] ?>" class="rounded w-full h-48 object-cover"> <!-- Muestra la imagen del producto -->
                 <h3 class="text-sm"><?= $prod['nombre'] ?></h3> <!-- Muestra el nombre del producto -->
                 <p class="font-bold text-lg">$<?= number_format($prod['precio'], 0, ',', '.') ?></p> <!-- Muestra el precio del producto --> 
                 <a href="index.php?controller=carrito&action=agregar&id=<?= $prod['id'] ?>" class="text-red-400 hover:underline text-sm">Comprar</a> <!-- Botón para agregar al carrito -->
